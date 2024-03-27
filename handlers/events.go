3f1bcb3fad5259e2d0d539d60db0ce2344342b15
bogdancanciu <bogdancanciu21@gmail.com>
2024-03-27 09:58:29 +0200

    Introduce attend event feature

--- handlers/events.go
+++ handlers/events.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
+	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/tools/types"
 	"net/http"
 )
 
@@ -24,4 +27,39 @@ func BindEventsHooks(app core.App) {
 
 		return nil
 	})
+	app.OnBeforeServe().Add(AttendEvent(app))
+}
+
+func AttendEvent(app core.App) func(e *core.ServeEvent) error {
+	return func(e *core.ServeEvent) error {
+		e.Router.POST("/api/events/:event_id", func(c echo.Context) error {
+			session := getSessionToken(c.Request())
+			userId, sessionErr := UserIdFromSession(session)
+			if sessionErr != nil {
+				return sessionErr
+			}
+
+			eventId := c.PathParam("event_id")
+			record, err := app.Dao().FindRecordById("events", eventId)
+			if err != nil {
+				return apis.NewApiError(http.StatusInternalServerError, "Server error", "")
+			}
+
+			var attendantsSlice []string
+			attendants := record.Get("attendants").(types.JsonRaw)
+			err = json.Unmarshal(attendants, &attendantsSlice)
+			if err != nil {
+				return apis.NewApiError(http.StatusInternalServerError, "Server error", "")
+			}
+
+			attendantsSlice = append(attendantsSlice, userId)
+			record.Set("attendants", attendantsSlice)
+			if err := app.Dao().SaveRecord(record); err != nil {
+				return apis.NewApiError(http.StatusInternalServerError, "Server error", "")
+			}
+
+			return c.NoContent(http.StatusOK)
+		})
+		return nil
+	}
 }
